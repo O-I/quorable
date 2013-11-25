8ca6a16b29eabc48a0b6ed59d33b1bcf61aaa823
Rahul Horé <hore.rahul@gmail.com>
2013-11-25 16:16:21 -0500

    Creates Quorable module

--- lib/quorable.rb
+++ lib/quorable.rb
@@ -2,13 +2,17 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-BASE_URL = 'http://www.quora.com'
+module Quorable
+  BASE_URL = 'http://www.quora.com'
 
-def parameterize(topic)
-  topic.split(' ').map(&:downcase).map(&:capitalize).join('-')
-end
+  def feed(topic)
+    doc = Nokogiri::HTML.parse(open("#{BASE_URL}/#{parameterize(topic)}"))
+    paths = doc.css('.question_link').map { |link| link['href'] }
+  end
 
-def feed(topic)
-  doc = Nokogiri::HTML.parse(open("#{BASE_URL}/#{parameterize(topic)}"))
-  paths = doc.css('.question_link').map { |link| link['href'] }
+  private
+
+  def parameterize(topic)
+    topic.split(' ').map(&:downcase).map(&:capitalize).join('-')
+  end
 end
