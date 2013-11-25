11e9a5f7d7bcaba1c4d90e0f29686d65367d5280
Rahul Horé <hore.rahul@gmail.com>
2013-11-25 16:06:51 -0500

    Cleans up quorable.rb

--- lib/quorable.rb
+++ lib/quorable.rb
@@ -8,18 +8,7 @@ def parameterize(topic)
   topic.split(' ').map(&:downcase).map(&:capitalize).join('-')
 end
 
-# topic = 'data structures'
-# topic = normalize(topic)
-
 def feed(topic)
   doc = Nokogiri::HTML.parse(open("#{BASE_URL}/#{parameterize(topic)}"))
-  #questions = doc.css('.link_text').map(&:text)
-  # expanded_texts = doc.css('.expanded_q_text').map { }
   paths = doc.css('.question_link').map { |link| link['href'] }
 end
-
-puts feed('ruby on rails')
-puts
-puts feed('gila monster')
-
-binding.pry
