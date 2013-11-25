d6d182a381acdcbf33d00538b1036aa56e633e15
Rahul Horé <hore.rahul@gmail.com>
2013-11-25 15:45:43 -0500

    Refines quorable.rb

--- lib/quorable.rb
+++ lib/quorable.rb
@@ -1,21 +1,25 @@
 require 'open-uri'
 require 'nokogiri'
-require 'json'
 require 'pry'
 
-def normalize(topic)
+BASE_URL = 'http://www.quora.com'
+
+def parameterize(topic)
   topic.split(' ').map(&:downcase).map(&:capitalize).join('-')
 end
 
-base_url = 'https://www.quora.com'
-topic = 'data structures'
-topic = normalize(topic)
+# topic = 'data structures'
+# topic = normalize(topic)
 
-doc = Nokogiri::HTML.parse(open("#{base_url}/#{topic}"))
-questions = doc.css('.link_text').map(&:text)
-# expanded_texts = doc.css('.expanded_q_text').map { }
-links = doc.css('.question_link').map { |link| link['href'] }
+def feed(topic)
+  doc = Nokogiri::HTML.parse(open("#{BASE_URL}/#{parameterize(topic)}"))
+  #questions = doc.css('.link_text').map(&:text)
+  # expanded_texts = doc.css('.expanded_q_text').map { }
+  paths = doc.css('.question_link').map { |link| link['href'] }
+end
 
-p questions, links
+puts feed('ruby on rails')
+puts
+puts feed('gila monster')
 
 binding.pry
