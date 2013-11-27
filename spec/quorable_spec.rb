04d9ba8120b5cdec75f8b236fa3f226f5e3941a3
Rahul Horé <hore.rahul@gmail.com>
2013-11-27 10:00:37 -0500

    Updates spec file

--- spec/quorable_spec.rb
+++ spec/quorable_spec.rb
@@ -5,7 +5,7 @@ describe Quorable do
     before do
       stub_request(:get, "http://www.quora.com/Music").
          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-         to_return(:status => 200, :body => "", :headers => {})
+         to_return(:status => 200, :body => fixture('sample_topic_response.rb'), :headers => {})
     end
     it 'returns an array of 10 or fewer paths to the Quora topic provided' do
       expect(Quorable.feed('music')).to eq []
