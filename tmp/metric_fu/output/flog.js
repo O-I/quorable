ed791c0b342e1c11a998eda25599e87d48545ddf
Rahul Horé <hore.rahul@gmail.com>
2013-11-27 10:13:14 -0500

    Adds .travis.yml file and metric_fu

---
+++ tmp/metric_fu/output/flog.js
@@ -0,0 +1,12 @@
+              var g = new Bluff.Line('graph', "1000x600");
+      g.theme_37signals();
+      g.tooltips = true;
+      g.title_font_size = "24px"
+      g.legend_font_size = "12px"
+      g.marker_font_size = "10px"
+
+        g.title = 'Flog: code complexity';
+        g.data('average', [6.073258726067112]);
+g.data('top 5% average', [12.000000000000002]);
+        g.labels = {"0":"11/27"};
+        g.draw();
